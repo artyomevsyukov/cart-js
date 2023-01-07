d3394a72bcdd685587b9dc58774ef4fbd2db8d07
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-01-07 19:35:15 +0300

    Added data and delete button

--- src/components/ButtonDelete/index.jsx
+++ src/components/ButtonDelete/index.jsx
@@ -1,6 +1,11 @@
-const ButtonDelete = () => {
+const ButtonDelete = ({ deleteProduct, id }) => {
   return (
-    <button type="button">
+    <button
+      type="button"
+      onClick={() => {
+        deleteProduct(id);
+      }}
+    >
       <img src="./img/icons/cross.svg" alt="Delete" />
     </button>
   );
--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import CartFooter from "../CartFooter";
 import CartHeader from "../CartHeader";
 import Product from "../Product";
+import data from "./../../data";
 
 const Cart = () => {
+  const [cart, setCart] = useState(data);
+
+  const deleteProduct = (id) => {
+    // console.log("Delete!!", id);
+    setCart((cart) => cart.filter((product) => id !== product.id));
+  };
+  // const deleteProduct = (id) => {
+  //   console.log("Delete!!", id);
+  //   setCart((cart) => {
+  //     return cart.filter((product) => { return id !== product.id;
+  //     });
+  //   });
+  // };
+
+  const products = cart.map((product) => {
+    return (
+      <Product
+        product={product}
+        key={product.id}
+        deleteProduct={deleteProduct}
+      />
+    );
+  });
+
   return (
     <section className="cart">
       <CartHeader />
 
-      <Product />
+      {products}
 
       <CartFooter />
     </section>
